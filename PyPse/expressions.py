affa5d2f5ad057a3b517ed6e62e57a4025a402c3
HankelBao <hankelbao@outlook.com>
2018-11-11 20:27:55 +0800

    clean up workspace for restart

--- PyPse/expressions.py
+++ PyPse/expressions.py
@@ -5,11 +5,13 @@ from .symbols import Symbols, SymbolType
 from .keys import Key
 import copy
 
+
 class Expression():
     def __init__(self, exp_str: str, block):
         self.exp_str = exp_str
         self.block = block
         exp_items = [exp_str]
+
         def divide_by_first_appearance(item: str, divider: str) -> (str, str):
             left_exp = item[:item.index(divider)].strip()
             right_exp = item[item.index(divider)+len(divider):].strip()
@@ -25,7 +27,8 @@ class Expression():
                     replaced_items.append(right_exp)
                 return replaced_items
 
-            replaced_items = combine_replaced_items(left_exp, mid_item, right_exp)
+            replaced_items = combine_replaced_items(
+                left_exp, mid_item, right_exp)
             exp_items[index:index+1] = replaced_items
 
         # Deal with Strings
@@ -33,11 +36,14 @@ class Expression():
             for index, exp_item in enumerate(exp_items):
                 if type(exp_item) == str:
                     if '"' in exp_item:
-                        left_exp, other_exp = divide_by_first_appearance(exp_item, '"')
-                        string_str, right_exp = divide_by_first_appearance(other_exp, '"')
+                        left_exp, other_exp = divide_by_first_appearance(
+                            exp_item, '"')
+                        string_str, right_exp = divide_by_first_appearance(
+                            other_exp, '"')
                         string_value = Value(ValueType.STRING)
                         string_value.assign_value_in_python(str(string_str))
-                        replace_str_with_items(left_exp, string_value, right_exp, index)
+                        replace_str_with_items(
+                            left_exp, string_value, right_exp, index)
                         return True
             return False
 
@@ -51,12 +57,14 @@ class Expression():
                 if type(exp_item) == str:
                     for OperationType in BasicOperationTypes:
                         if OperationType.identifier in exp_item:
-                            left_exp, right_exp = divide_by_first_appearance(exp_item, OperationType.identifier)
+                            left_exp, right_exp = divide_by_first_appearance(
+                                exp_item, OperationType.identifier)
 
                             operation = Operation(OperationType)
                             operation.increase_priority(OperationType.priority)
 
-                            replace_str_with_items(left_exp, operation, right_exp, index)
+                            replace_str_with_items(
+                                left_exp, operation, right_exp, index)
                             return True
             return False
 
@@ -88,15 +96,19 @@ class Expression():
                     number_of_right_brackets_at_end += 1
 
                 number_of_left_operation_brackets = number_of_left_brackets_at_beginning
-                number_of_right_operation_brackets = number_of_right_brackets_at_end - (number_of_left_brackets - number_of_left_brackets_at_beginning)
+                number_of_right_operation_brackets = number_of_right_brackets_at_end - \
+                    (number_of_left_brackets - number_of_left_brackets_at_beginning)
 
-                degree_increase_for_brackets += OperationTypeLeftBracket.priority * number_of_left_operation_brackets
-                degree_increase_for_brackets += OperationTypeRightBracket.priority * number_of_right_operation_brackets
+                degree_increase_for_brackets += OperationTypeLeftBracket.priority * \
+                    number_of_left_operation_brackets
+                degree_increase_for_brackets += OperationTypeRightBracket.priority * \
+                    number_of_right_operation_brackets
 
                 if number_of_right_operation_brackets == 0:
                     exp_items[index] = exp_item[number_of_left_operation_brackets:]
                 else:
-                    exp_items[index] = exp_item[number_of_left_operation_brackets: -number_of_right_operation_brackets]
+                    exp_items[index] = exp_item[number_of_left_operation_brackets: -
+                                                number_of_right_operation_brackets]
 
             if type(exp_item) == Operation:
                 exp_item.priority += degree_increase_for_brackets
@@ -104,8 +116,7 @@ class Expression():
         # Get Rid of the spaces
         for index, exp_item in enumerate(exp_items):
             if type(exp_item) == str:
-               exp_items[index] = exp_item.strip()
-
+                exp_items[index] = exp_item.strip()
 
         self.exp_items = exp_items
 
@@ -127,15 +138,18 @@ class Expression():
                         exp_items[index] = value
                 elif "(" in exp_item and ")" in exp_item:
                     symbol_name = exp_item[:exp_item.index("(")]
-                    function_symbol = self.block.symbols.get(SymbolType.FUNCTION, symbol_name)
+                    function_symbol = self.block.symbols.get(
+                        SymbolType.FUNCTION, symbol_name)
 
-                    param_strs = exp_item[exp_item.index("(")+1: exp_item.index(")")]
+                    param_strs = exp_item[exp_item.index(
+                        "(")+1: exp_item.index(")")]
                     param_values = []
                     for param_str in param_strs.split(","):
                         param_str = param_str.strip(" ")
                         value = Expression(param_str, self.block).get_value()
                         param_values.append(value)
-                    function_block = copy.deepcopy(function_symbol.symbol_value.value_in_python)
+                    function_block = copy.deepcopy(
+                        function_symbol.symbol_value.value_in_python)
                     return_value = function_block.call(param_values)
                     exp_items[index] = return_value
                 else:
@@ -157,8 +171,10 @@ class Expression():
             highest_priority_operation = exp_items[max_priority_index]
             right_item = exp_items[max_priority_index+1]
 
-            result_value = highest_priority_operation.OperationType.operate(left_item, right_item)
-            exp_items[max_priority_index-1: max_priority_index+2] = [result_value]
+            result_value = highest_priority_operation.OperationType.operate(
+                left_item, right_item)
+            exp_items[max_priority_index -
+                      1: max_priority_index+2] = [result_value]
 
         while len(exp_items) > 1:
             calculate_highest_operation_once()
